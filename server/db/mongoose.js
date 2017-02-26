a32723f02e37c0cb0cf7467ebacf44348c0696fe
Heinrich Latreider <heinrich.latreider@haw-hamburg.de>
2017-02-26 14:48:43 +0100

    setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://mintvm:27017/todoApp");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://mintvm:27017/todoApp");
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
 var app = express();
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
@@ -44,8 +46,8 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("listen on port 3000");
+app.listen(port, () => {
+    console.log("listen on port", port);
 });
 
 module.exports = {app};
